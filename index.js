9671d097fcee5d986b10666123bd632743a57e63
xuan truongm <tranxuantruong203hn@gmail.com>
2022-09-05 15:36:58 +0700

    4

--- index.js
+++ index.js
@@ -10,25 +10,26 @@ const resolvers = require("./resolvers");
 const { permissions } = require("./permissions");
 // Api
 const AccountAPI = require("./datasources/AccountApi");
-
-const server = new ApolloServer({
-  schema: applyMiddleware(buildSubgraphSchema({ typeDefs, resolvers }), permissions),
-  context: ({ req }) => {
-    const user = req.headers.auth ? JSON.parse(req.headers.auth) : null;
-    const dataSources = {
-      accountAPI: new AccountAPI(),
-    };
-    return { user, dataSources };
-  },
-});
-
 const subgraphName = "users";
-
-server
-  .listen({ port: process.env.PORT || 4001 })
-  .then(({ url }) => {
-    console.log(`🚀 Subgraph ${subgraphName} running at ${url}`);
-  })
-  .catch((err) => {
-    console.error(err);
+async function startServer() {
+  const server = new ApolloServer({
+    schema: applyMiddleware(buildSubgraphSchema({ typeDefs, resolvers }), permissions),
+    context: ({ req }) => {
+      const user = req.headers.auth ? JSON.parse(req.headers.auth) : null;
+      const dataSources = {
+        accountAPI: new AccountAPI(),
+      };
+      return { user, dataSources };
+    },
   });
+
+  const { url, port } = await server.listen({
+    port: process.env.PORT || 4000,
+  });
+  console.log(`
+      🚀  Server is running
+      🔉  Listening on port ${port}
+      📭  Query at ${url}
+      `);
+}
+startServer(typeDefs, resolvers);
